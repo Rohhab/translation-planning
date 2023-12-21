925d4f46a0795b3a9c44f6b019a006471c4a8303
Rohhab <S.Pooria.hosseini@gmail.com>
2023-12-21 17:20:05 +0330

    Wrong implementation of Home and Login page

--- translation-planning-ui/src/App.js
+++ translation-planning-ui/src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
@@ -36,7 +10,6 @@ function App() {
       .get('http://localhost:3100')
       .then((response) => {
         setMessage(response.data);
-        console.log(response);
       })
       .catch((error) => {
         console.error(error);
@@ -47,9 +20,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -59,10 +30,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <h1>{message}</h1>
     </div>
-    // <div>
-    // </div>
   );
 }
 
