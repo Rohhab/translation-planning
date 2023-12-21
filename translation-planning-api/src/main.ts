e85e84d642b85b32a731b71e07d6789384794f6b
Rohhab <S.Pooria.hosseini@gmail.com>
2023-12-21 16:39:55 +0330

    Integrate React with NestJS

--- translation-planning-api/src/main.ts
+++ translation-planning-api/src/main.ts
@@ -4,12 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  // Shall the port be reverted to 3000?
   await app.listen(3100);
 }
 bootstrap();
