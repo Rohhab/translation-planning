13786b55c0c5a34f0614df7f5cc7715e58c15976
rohhab <s.pooria.hosseini@gmail.com>
2024-01-04 00:42:24 +0330

    Handle the routes and navigation

--- translation-planning-ui/src/index.js
+++ translation-planning-ui/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/home-page/App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginFunc from './pages/login-page/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'auth/login',
+    element: <LoginFunc />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- translation-planning-ui/src/pages/login-page/Login.js
+++ translation-planning-ui/src/pages/login-page/Login.js
@@ -4,9 +4,9 @@ import { useRef } from 'react';
 //import { useState } from 'react';
 //import axios from 'axios';
 import toltec from './toltec.webp';
-import './App.css';
+import './Login.css';
 
-function App() {
+function LoginFunc() {
   const textBoxRef = useRef(null);
 
   useEffect(() => {
@@ -29,33 +29,28 @@ function App() {
   // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="Login">
+      <header className="Login-header">
         <img src={toltec} alt="The Toltec Warrior" />
         {/* <p>{message}</p> */}
-        <p ref={textBoxRef}>
-          An average man cares that things are either true or false, but a
-          <i> warrior</i> doesn't. <br />
-          An average man proceeds in a specific way with things that he knows
-          are true, and in a different way with things that he knows are not
-          true. If things are said to be true, he acts and believes in what he
-          does. But if things are said to be untrue, he doesn't care to act, or
-          he doesn't believe in what he does. <br />A <i> warrior</i>, on the
-          other hand, acts in both instances. If things are said to be true, he
-          would act in order to do doing. If things are said to be untrue, he
-          still would act in order to do not-doing. <br />
-          See what I mean?
-        </p>
+        <p ref={textBoxRef}>The new one!</p>
         <button
           onClick={() => {
             window.location.href = 'http://localhost:3100';
           }}
         >
-          Login
+          Login / Sign up
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = '/';
+          }}
+        >
+          Home Page
         </button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default LoginFunc;
