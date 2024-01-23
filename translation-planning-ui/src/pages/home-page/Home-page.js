e00c4899a73f248535bea771892b22be220778ef
rohhab <s.pooria.hosseini@gmail.com>
2024-01-23 22:58:13 +0330

    Complete the UI of login page

--- translation-planning-ui/src/pages/home-page/Home-page.js
+++ translation-planning-ui/src/pages/home-page/Home-page.js
@@ -16,24 +16,26 @@ const HomePage = () => {
             className="Typewriter"
             sequence={[
               `An average man cares that things are either true or false, but a warrior doesn't.`,
-              1000,
+              3000,
               `An average man proceeds in a specific way with things that he knows are true, and in a different way with things that he knows are not true.`,
-              1000,
+              3000,
               `If things are said to be true, he acts and believes in what he does. But if things are said to be untrue, he doesn't care to act, or he doesn't believe in what he does.`,
-              1000,
+              3000,
               `A warrior, on the other hand, acts in both instances. If things are said to be true, he would act in order to do doing. If things are said to be untrue, he still would act in order to do not-doing.`,
-              1000,
+              3000,
               `See what I mean?`,
-              2000,
+              4000,
+              '',
+              1000,
             ]}
             speed={55}
-            deletionSpeed={85}
+            deletionSpeed={95}
             repeat={Infinity}
             style={{
               fontFamily: 'Gill Sans',
-              fontSize: 28,
-              textShadow: 'auto',
-              color: 'lightblue',
+              fontSize: 40,
+              textShadow: '2px 0 1px blue, -2px 0 1px #ff6600',
+              color: '#80bfff',
             }}
           />
         </div>
--- translation-planning-ui/src/pages/login-page/Login.js
+++ translation-planning-ui/src/pages/login-page/Login.js
@@ -1,262 +1,338 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { useState } from 'react';
+import { useFormik } from 'formik';
 import axios from 'axios';
-import toltec from '../toltec.webp';
 import './Login.css';
 
-const formTypes = {
-  SIGNIN_USERNAME: 'signin_username',
-  SIGNIN_EMAIL: 'signin_email',
-  SIGNUP: 'signup',
-};
-
 function Login() {
-  const [formType, setFormType] = useState(null);
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  const [responseMessage, setResponseMessage] = useState('');
+  const [selectedForm, setSelectedForm] = useState('');
 
-  const handleFormTypeChange = (type) => {
-    setFormType(type);
-    setFormData({
-      name: '',
-      email: '',
-      password: '',
-    });
-    setResponseMessage('');
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (formType === formTypes.SIGNIN_USERNAME) {
+  const handleSubmit = (values) => {
+    if (selectedForm === 'SIGNUP') {
       axios
-        .post('http://localhost:3100/auth/login/signin', formData)
+        .post('http://localhost:3100/auth/login/signup', values)
         .then((response) => {
-          console.log('Response from API:', response.data);
-
-          setResponseMessage(JSON.stringify(response.data));
-          window.location = '/books';
-        })
-        .catch((error) => {
-          console.error('Error:', error.response.data.message);
-
-          setResponseMessage(JSON.stringify(error.response.data.message));
-        });
-    } else if (formType === formTypes.SIGNIN_EMAIL) {
-      axios
-        .post('http://localhost:3100/auth/login/signin', formData)
-        .then((response) => {
-          console.log('Response from API:', response.data);
-
-          setResponseMessage(JSON.stringify(response.data));
-        })
-        .catch((error) => {
-          console.error('Error:', error.response.data.message);
-
-          setResponseMessage(JSON.stringify(error.response.data.message));
-        });
-    } else if (formType === formTypes.SIGNUP) {
-      axios
-        .post('http://localhost:3100/auth/login/signup', formData)
-        .then((response) => {
-          console.log('Response from API:', response.data);
-
-          setResponseMessage(JSON.stringify(response.data));
+          console.log('Response from the API:', response.data);
         })
         .catch((error) => {
           console.error('Error:', error);
-
-          setResponseMessage(JSON.stringify(error.response.data.message));
         });
+      console.log('Form submitted with values:', values);
     }
   };
+  //     axios
+  //       .post('http://localhost:3100/auth/login/signup', formData)
+  //       .then((response) => {
+  //         console.log('Response from API:', response.data);
 
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
+  //         setResponseMessage(JSON.stringify(response.data));
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error:', error);
+
+  //         setResponseMessage(JSON.stringify(error.response.data.message));
+  //       });
+  //   }
+
+  const Form = () => {
+    const formik = useFormik({
+      initialValues: {
+        name: '',
+        email: '',
+        password: '',
+      },
+      onSubmit: handleSubmit,
+      // validate: (values) => {
+      //   let errors = {};
+      //   if (selectedForm === 'SIGNUP' || selectedForm === 'SIGNIN_USERNAME') {
+      //     if (!values.username) {
+      //       errors.username = 'Required';
+      //     }
+      //   }
+      //   if (selectedForm === 'SIGNUP' || selectedForm === 'SIGNIN_EMAIL') {
+      //     if (!values.email) {
+      //       errors.email = 'Required';
+      //     }
+      //   }
+      //   if (!values.password) {
+      //     errors.password = 'Required';
+      //   }
+      //   return errors;
+      // },
     });
-  };
 
-  const renderFormFields = () => {
-    if (formType === formTypes.SIGNIN_USERNAME) {
-      return (
-        <>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <button type="submit" className="Login-button">
-            {formType === formTypes.SIGNIN_USERNAME
-              ? 'Sign In with Username'
-              : formType === formTypes.SIGNIN_EMAIL
-              ? 'Sign In with Email'
-              : 'Sign Up'}
-          </button>
-        </>
-      );
-    } else if (formType === formTypes.SIGNIN_EMAIL) {
-      return (
-        <>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <button type="submit" className="Login-button">
-            {formType === formTypes.SIGNIN_USERNAME
-              ? 'Sign In with Username'
-              : formType === formTypes.SIGNIN_EMAIL
-              ? 'Sign In with Email'
-              : 'Sign Up'}
-          </button>
-        </>
-      );
-    } else if (formType === formTypes.SIGNUP) {
-      return (
-        <>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <button type="submit" className="Login-button">
-            {formType === formTypes.SIGNIN_USERNAME
-              ? 'Sign In with Username'
-              : formType === formTypes.SIGNIN_EMAIL
-              ? 'Sign In with Email'
-              : 'Sign Up'}
-          </button>
-        </>
-      );
-    }
+    return (
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Username:</label>
+        <input
+          id="name"
+          name="name"
+          type="username"
+          placeholder="Enter your username"
+          disabled={
+            selectedForm === 'SIGNUP'
+              ? false
+              : selectedForm === 'SIGNIN_USERNAME'
+              ? false
+              : true
+          }
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
+        {/* {formik.errors.username && formik.touched.username && (
+          <div>{formik.errors.username}</div>
+        )} */}
 
-    return null;
+        <label htmlFor="email">Email Address:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Enter your E-mail address"
+          disabled={
+            selectedForm === 'SIGNUP'
+              ? false
+              : selectedForm === 'SIGNIN_EMAIL'
+              ? false
+              : true
+          }
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {/* {formik.errors.email && formik.touched.email && (
+          <div>{formik.errors.email}</div>
+        )} */}
+
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        {/* {formik.errors.password && formik.touched.password && (
+          <div>{formik.errors.password}</div>
+        )} */}
+
+        <button type="submit">Submit</button>
+      </form>
+    );
   };
 
   return (
-    <div className="Container">
-      <div>
+    <div className="LoginPage">
+      <div className="LoginPage__background"></div>
+      <div className="LoginPage__sidebar">
+        <a href="/">Home Page</a>
+        <button onClick={() => setSelectedForm('SIGNIN_USERNAME')}>
+          Sign in with username
+        </button>
+        <button onClick={() => setSelectedForm('SIGNIN_EMAIL')}>
+          Sign in with email
+        </button>
+        <button onClick={() => setSelectedForm('SIGNUP')}>Sign up</button>
+      </div>
+      <div className="LoginPage__content">
         <Helmet>
           <title>Login to TranslationPlanning</title>
         </Helmet>
-      </div>
 
-      <div className="Image-container">
-        <div className="Background-image">
-          <img src={toltec} alt="The Toltec Warrior" />
-        </div>
-      </div>
-      <div className="Left-column">
-        <div className="Centered-content">
-          <p
-            className={`toggle-link ${
-              formType === formTypes.SIGNIN_USERNAME ? 'active' : ''
-            }`}
-            onClick={() => handleFormTypeChange(formTypes.SIGNIN_USERNAME)}
-          >
-            Sign in with username
-          </p>
-          <p
-            className={`toggle-link ${
-              formType === formTypes.SIGNIN_EMAIL ? 'active' : ''
-            }`}
-            onClick={() => handleFormTypeChange(formTypes.SIGNIN_EMAIL)}
-          >
-            Sign in with email
-          </p>
-          <p
-            className={`toggle-link ${
-              formType === formTypes.SIGNUP ? 'active' : ''
-            }`}
-            onClick={() => handleFormTypeChange(formTypes.SIGNUP)}
-          >
-            Sign up
-          </p>
-          <form onSubmit={handleSubmit}>{renderFormFields()}</form>
-        </div>
-      </div>
-      <div className="Right-column">
-        <button
-          className="Home-button"
-          onClick={() => {
-            window.location.href = '/';
-          }}
-        >
-          Home Page
-        </button>
-        {responseMessage && <p>{responseMessage}</p>}
+        <Form />
       </div>
     </div>
   );
 }
 
 export default Login;
+
+// const [formType, setFormType] = useState(null);
+// const [formData, setFormData] = useState({
+//   name: '',
+//   email: '',
+//   password: '',
+// });
+// const [responseMessage, setResponseMessage] = useState('');
+
+// const handleFormTypeChange = (type) => {
+//   setFormType(type);
+//   setFormData({
+//     name: '',
+//     email: '',
+//     password: '',
+//   });
+//   setResponseMessage('');
+// };
+
+// const handleSubmit = async (event) => {
+//   event.preventDefault();
+
+//   if (formType === formTypes.SIGNIN_USERNAME) {
+//     axios
+//       .post('http://localhost:3100/auth/login/signin', formData)
+//       .then((response) => {
+//         console.log('Response from API:', response.data);
+
+//         setResponseMessage(JSON.stringify(response.data));
+//         window.location = '/books';
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error.response.data.message);
+
+//         setResponseMessage(JSON.stringify(error.response.data.message));
+//       });
+//   } else if (formType === formTypes.SIGNIN_EMAIL) {
+//     axios
+//       .post('http://localhost:3100/auth/login/signin', formData)
+//       .then((response) => {
+//         console.log('Response from API:', response.data);
+
+//         setResponseMessage(JSON.stringify(response.data));
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error.response.data.message);
+
+//         setResponseMessage(JSON.stringify(error.response.data.message));
+//       });
+//   } else if (formType === formTypes.SIGNUP) {
+//     axios
+//       .post('http://localhost:3100/auth/login/signup', formData)
+//       .then((response) => {
+//         console.log('Response from API:', response.data);
+
+//         setResponseMessage(JSON.stringify(response.data));
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+
+//         setResponseMessage(JSON.stringify(error.response.data.message));
+//       });
+//   }
+// };
+
+// const handleChange = (event) => {
+//   setFormData({
+//     ...formData,
+//     [event.target.name]: event.target.value,
+//   });
+// };
+
+// const renderFormFields = () => {
+//   if (formType === formTypes.SIGNIN_USERNAME) {
+//     return (
+//       <>
+//         <label>
+//           Name:
+//           <input
+//             type="text"
+//             name="name"
+//             placeholder="Name"
+//             value={formData.name}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             value={formData.password}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <button type="submit" className="Login-button">
+//           {formType === formTypes.SIGNIN_USERNAME
+//             ? 'Sign In with Username'
+//             : formType === formTypes.SIGNIN_EMAIL
+//             ? 'Sign In with Email'
+//             : 'Sign Up'}
+//         </button>
+//       </>
+//     );
+//   } else if (formType === formTypes.SIGNIN_EMAIL) {
+//     return (
+//       <>
+//         <label>
+//           Email:
+//           <input
+//             type="email"
+//             name="email"
+//             placeholder="Email"
+//             value={formData.email}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             value={formData.password}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <button type="submit" className="Login-button">
+//           {formType === formTypes.SIGNIN_USERNAME
+//             ? 'Sign In with Username'
+//             : formType === formTypes.SIGNIN_EMAIL
+//             ? 'Sign In with Email'
+//             : 'Sign Up'}
+//         </button>
+//       </>
+//     );
+//   } else if (formType === formTypes.SIGNUP) {
+//     return (
+//       <>
+//         <label>
+//           Name:
+//           <input
+//             type="text"
+//             name="name"
+//             placeholder="Name"
+//             value={formData.name}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Email:
+//           <input
+//             type="email"
+//             name="email"
+//             placeholder="Email"
+//             value={formData.email}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             value={formData.password}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <br />
+//         <button type="submit" className="Login-button">
+//           {formType === formTypes.SIGNIN_USERNAME
+//             ? 'Sign In with Username'
+//             : formType === formTypes.SIGNIN_EMAIL
+//             ? 'Sign In with Email'
+//             : 'Sign Up'}
+//         </button>
+//       </>
+//     );
+//   }
+
+//   return null;
+// };
